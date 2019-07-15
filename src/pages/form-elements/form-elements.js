15620d23d0130e9dd5a835935e70a78b29e776f0
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2019-07-15 10:42:26 +0600

    Верстка Form-elements
    Верстка UI Kit - Form-elements

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,3 +1,7 @@
 import '../../../node_modules/normalize.css/normalize.css';
 import './form-elements.scss';
+
+import '../../plugins/jquery-ui.min.js';
+
 import '../../blocks/toggle/toggle.js';
+import '../../blocks/range-slider/range-slider.js';
