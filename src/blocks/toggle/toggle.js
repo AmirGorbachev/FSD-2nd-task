f537b2ccbc8d36b7920c17c7ddcac52dfc321723
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2019-07-15 06:28:36 +0600

    Верстка Form-elements
    Начал вертску:
    - кнопка "лайка"
    - рейтинг
    - переключатель
    Пока черновой вариант. Подключил jQuery.

---
+++ src/blocks/toggle/toggle.js
@@ -0,0 +1,12 @@
+var $ = require("jquery");
+
+$(function () {
+    $('.switch-btn').click(function () {
+        $(this).toggleClass('switch-on');
+        if ($(this).hasClass('switch-on')) {
+            $(this).trigger('on.switch');
+        } else {
+            $(this).trigger('off.switch');
+        }
+    });
+});
--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -1,2 +1,3 @@
 import '../../../node_modules/normalize.css/normalize.css';
 import './form-elements.scss';
+import '../../blocks/toggle/toggle.js';
