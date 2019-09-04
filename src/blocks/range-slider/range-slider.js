c381303ae403300cc346eab804666f899888bb7f
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2019-09-05 01:19:50 +0600

    Небольшие доработки стилей

--- src/blocks/range-slider/range-slider.js
+++ src/blocks/range-slider/range-slider.js
@@ -4,7 +4,8 @@ $( function() {
     $( ".slider-range" ).slider({
       range: true,
       min: 0,
-      max: 15706,
+      max: 15700,
+      step: 10,
       values: [ 5000, 10000 ],
       slide: function( event, ui ) {
         $( ".range-slider__label" ).val( ui.values[ 0 ] + "Р - " + ui.values[ 1 ] + "Р");
