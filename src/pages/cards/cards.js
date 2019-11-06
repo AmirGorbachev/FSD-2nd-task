4ab30915eb8d41a29abd73cd81505e033a5a5f50
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2019-11-06 10:56:39 +0600

    Верстка страницы Cards

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,2 +1,9 @@
 import '../../../node_modules/normalize.css/normalize.css';
 import './cards.scss';
+/* Плагин - jQuery UI */
+import '../../plugins/jquery-ui/jquery-ui.min.js';
+/* Плагин - AIR DATEPICKER  */
+import '../../../node_modules/air-datepicker/dist/js/datepicker.min.js';
+/* Подключение скриптов БЭМ-блоков */
+import '../../blocks/calendar/calendar.js';
+import '../../blocks/toggle/toggle.js';
