6b19cad2ebb9d61a00c4b63aaa6d837ec69928b0
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2020-03-23 21:33:57 +0600

    Формы слов для dropdown
    Корректное изменение форм слов ("спальня", "спальни", "спален" и т.д. для каждого пункта дропдауна)

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -44,13 +44,56 @@ document.addEventListener('DOMContentLoaded', function(){
 document.addEventListener('DOMSubtreeModified', function(){
   // Собираем все dropdown на странице
   const dropdowns = document.querySelectorAll('.dropdown');
+  let bedrooms, beds, bathrooms; // написание спален, кроватей и ванн в зависимости от количества
   // Работаем с каждым dropdown отдельно
   for(let i=0; i<dropdowns.length; i++) {
     const input = dropdowns[i].childNodes[0].childNodes[0]; // Находим input
     const dropOutItems = dropdowns[i].querySelectorAll('.drop-out__amount'); // Массив значений из 3 счётчиков
     // Записываем значения в input
     if((dropOutItems[0].textContent != 0)||(dropOutItems[1].textContent != 0)||(dropOutItems[2].textContent != 0)) {
-      input.value = dropOutItems[0].textContent+' спальни, '+dropOutItems[1].textContent+' кровати, '+dropOutItems[2].textContent+' ванные комнаты';
+      // Формы слова "спальня"
+      switch(dropOutItems[0].textContent){
+        case "1": 
+          bedrooms = "спальня";
+          break;
+        case "2":
+        case "3":
+        case "4":
+          bedrooms = "спальни";
+          break;
+        default:
+          bedrooms = "спален";
+          break;
+      }
+      // Формы слова "кровать"
+      switch(dropOutItems[1].textContent){
+        case "1": 
+          beds = "кровать";
+          break;
+        case "2":
+        case "3":
+        case "4":
+          beds = "кровати";
+          break;
+        default:
+          beds = "кроватей";
+          break;
+      }
+      // Формы слова "ванная"
+      switch(dropOutItems[2].textContent){
+        case "1": 
+          bathrooms = "ванная комната";
+          break;
+        case "2":
+        case "3":
+        case "4":
+          bathrooms = "ванные комнаты";
+          break;
+        default:
+          bathrooms = "ванных комнат";
+          break;
+      }
+      input.value = dropOutItems[0].textContent+' '+bedrooms+', '+dropOutItems[1].textContent+' '+beds+', '+dropOutItems[2].textContent+' '+bathrooms;
     } else {
      input.value = null; // Если все счётчики "0", обнуляем значение input
     }
