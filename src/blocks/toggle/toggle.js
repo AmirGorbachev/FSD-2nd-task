ea1133d8a0fa4436dbe5c2361a53e2b4e537d24c
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2020-05-10 16:07:02 +0600

    Доработка toggle

--- src/blocks/toggle/toggle.js
+++ src/blocks/toggle/toggle.js
@@ -5,3 +5,8 @@ $(function () {
         $(this).toggleClass('toggle__switch--on');
     });
 });
+$(function () {
+    $('.toggle__value').click(function () {
+        $(this).parent('.toggle__inner').children('.toggle__switch').toggleClass('toggle__switch--on');
+    });
+});
