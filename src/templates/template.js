f2f312c31b8846fc0abea654a9033d8c844b7eb8
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2020-08-08 22:11:36 +0600

    Новая сборка webpack

--- src/templates/template.js
+++ src/templates/template.js
@@ -1,8 +1,8 @@
 /* Сброс стилей */
 import "../../node_modules/normalize.css/normalize.css";
 /* Плагин - jQuery UI */
-import "../plugins/jquery-ui/jquery-ui.min.js";
 var $ = require("jquery");
+import "../plugins/jquery-ui/jquery-ui.min.js";
 /* Плагин - AIR DATEPICKER  */
 import "../../node_modules/air-datepicker/dist/js/datepicker.min.js";
 /* Подключение скриптов БЭМ-блоков */
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
-const autoprefixer = require('autoprefixer');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const webpack = require('webpack');
 
 module.exports = {
@@ -8,111 +8,66 @@ module.exports = {
     'headers&footers': __dirname + '/src/pages/headers&footers/headers&footers.js',
     'form-elements': __dirname + '/src/pages/form-elements/form-elements.js',
     'cards': __dirname + '/src/pages/cards/cards.js'
-  }, // webpack entry point. Module to start building dependency graph
+  },
   output: {
-    path: __dirname + '/dist', // Folder to store generated bundle
-    filename: '[name].js',  // Name of generated bundle after build
-    publicPath: '/' // public URL of the output directory when referenced in a browser
+    filename: '[name].[contenthash].js',
+    path: __dirname + '/dist'
   },
-  module: {  // where we defined file patterns and their loaders
-      rules: [
-          {
-            test: /\.(sass|scss)$/,
-            use: [{
-                loader: 'style-loader' // creates style nodes from JS strings
-            }, {
-                loader: 'css-loader' // translates CSS into CommonJS
-            }, {
-                loader: 'postcss-loader',
-                options: {
-                    plugins: [
-                          autoprefixer({
-                              browsers:['ie >= 8', 'last 4 version']
-                          })
-                    ],
-                    sourceMap: true
-                }
-            }, {
-                loader: 'sass-loader' // compiles Sass to CSS
-            }]
-          },
-          {
-            test: /\.css$/,
-            use: [{ 
-                loader: 'style-loader' 
-            }, { 
-                loader: 'css-loader' 
-            }, {
-                loader: 'postcss-loader',
-                options: {
-                    plugins: [
-                          autoprefixer({
-                              browsers:['ie >= 8', 'last 4 version']
-                          })
-                    ],
-                    sourceMap: true
-                }
-            }]
-          },
-          {
-            test: /\.pug$/,
-            use: [{
-                loader: 'pug-loader'
-            }]
-          },
-          {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            use: [{
-                loader: 'file-loader'
-            }]
-          },
-          {
-            test: /\.png$/,
-            use: [{
-                loader: 'file-loader'
-            }]
-          },
-          {
-            test: /\.(jpe?g|gif|jpg|png)$/,
-            use: [{
-                loader: 'file-loader'
-            }]
-          }
-      ]
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
   },
-  plugins: [  // Array of plugins to apply to build chunk
-      new HtmlWebpackPlugin({
-          template: __dirname + '/src/pages/colors&type/colors&type.pug',
-          filename: 'colors&type.html',
-          inject: 'head',
-          chunks: ['<index>']
-      }),
-      new HtmlWebpackPlugin({
-          template: __dirname + '/src/pages/headers&footers/headers&footers.pug',
-          filename: 'headers&footers.html',
-          inject: 'head',
-          chunks: ['<index>']
-      }),
-      new HtmlWebpackPlugin({
-          template: __dirname + '/src/pages/form-elements/form-elements.pug',
-          filename: 'form-elements.html',
-          inject: 'head',
-          chunks: ['<index>']
-      }),
-      new HtmlWebpackPlugin({
-          template: __dirname + '/src/pages/cards/cards.pug',
-          filename: 'cards.html',
-          inject: 'head',
-          chunks: ['<index>']
-      }),
-      new webpack.ProvidePlugin({
-        $: "jquery/dist/jquery.min.js",
-        jQuery: "jquery/dist/jquery.min.js",
-        "window.jQuery": "jquery/dist/jquery.min.js"
-      })
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: __dirname + '/src/pages/colors&type/colors&type.pug',
+      filename: 'colors&type.html'
+    }),
+    new HTMLWebpackPlugin({
+      template: __dirname + '/src/pages/headers&footers/headers&footers.pug',
+      filename: 'headers&footers.html'
+    }),
+    new HTMLWebpackPlugin({
+      template: __dirname + '/src/pages/form-elements/form-elements.pug',
+      filename: 'form-elements.html'
+    }),
+    new HTMLWebpackPlugin({
+      template: __dirname + '/src/pages/cards/cards.pug',
+      filename: 'cards.html'
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery/dist/jquery.min.js",
+      jQuery: "jquery/dist/jquery.min.js",
+      "window.jQuery": "jquery/dist/jquery.min.js"
+    }),
+    new CleanWebpackPlugin()
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.pug$/,
+        use: ['pug-loader']
+      },
+      {
+        test: /\.(jpe?g|gif|jpg|png|svg)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
   devServer: {  // configuration for webpack-dev-server 
       contentBase: './src/pages',  //source of static assets
       port: 7700, // port to run dev-server
   } 
-};
+}
