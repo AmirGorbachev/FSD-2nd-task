479f72d41c3358a08277bc3c9d4f2365e46b8906
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2020-08-24 17:57:55 +0600

    Add bg-img slider

--- src/templates/template.js
+++ src/templates/template.js
@@ -14,3 +14,33 @@ import "../blocks/like-button/like-button.js";
 import "../blocks/range-slider/range-slider.js";
 import "../blocks/toggle/toggle.js";
 import "../blocks/room-card/room-card.js";
+
+
+// Слайдер bg-img
+let imageBlocks = document.querySelectorAll(".slider");
+// Если элементам изначально задать в стилях displey: none;
+// то по неизвестным причинам функция fadeIn() не работает,
+// так что в начале скрываем все элементы с помощью js
+for(let i = 0; i <= imageBlocks.length; i++) {
+  $(imageBlocks[i]).fadeOut(0);
+}
+
+slideshow(imageBlocks);
+
+function slideshow(images) {
+  let nextImage = 0;
+  doSlideshow();
+  // Рекурсия для бесконечного листания
+  function doSlideshow(){
+    $(images[nextImage]).fadeOut(500);
+
+    nextImage += 1;
+    if(nextImage === images.length) {
+      nextImage = 0;
+    }
+
+    $(images[nextImage]).fadeIn(500);
+
+    setTimeout(doSlideshow, 3000);
+  }
+}
